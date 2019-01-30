fb0b90f89c9f75105268949b7cf337b20812a31d
Tsvetoslav Todorov <cecko_vr@abv.bg>
2019-01-30 18:45:57 +0200

    find the offset from e,t,h

--- src/DecodeCaesarCipher.java
+++ src/DecodeCaesarCipher.java
@@ -4,7 +4,42 @@ public class DecodeCaesarCipher {
         String cipherText = "H svun aptl hnv pu h nhshef mhy, mhy hdhf....";
         System.out.println(mostCommonLetter(cipherText));
 
+        char[] mostPresent = {'e', 't', 'h'};
+        int difference = 0;
+        cipherText = cipherText.toLowerCase();
+
+        System.out.println(" ----------------------------- ");
+        System.out.println("Most common letter in the text is : " + mostCommonLetter(cipherText));
+        System.out.println(" ----------------------------- ");
+        for (int i = 0; i < mostPresent.length; i++) {
+            difference = (-1) * ((int) ((char) (mostCommonLetter(cipherText))) - (int) mostPresent[i]);
+            System.out.println(shiftLetters(cipherText, difference));
+            //System.out.print(difference);
+        }
+
     }
+
+    public static String shiftLetters(String text, int diff) {
+
+        text = text.trim().toLowerCase();
+        String encodedText = "";
+        char[] arrLetters = text.toCharArray();
+
+        for (int i = 0; i < text.length(); i++) {
+            //a = 97 <=> z = 122    space = 32     dot[.] = 46  ASCII
+            if (arrLetters[i] >= 'a' && arrLetters[i] <= 'z') {
+                arrLetters[i] = (char) (arrLetters[i] - diff);
+                if (arrLetters[i] < 'a') {
+                    arrLetters[i] = (char) (arrLetters[i] + 'z' - 'a' + 1);
+                } else if (arrLetters[i] > 'z') {
+                    arrLetters[i] = (char) (arrLetters[i] + 'a' - 'z' - 1);
+                }
+            }
+            encodedText += arrLetters[i];
+        }
+        return encodedText;
+    }
+
     public static char mostCommonLetter(String text) {
         text = text.trim().toLowerCase();
         char[] arrLetters = text.toCharArray();
